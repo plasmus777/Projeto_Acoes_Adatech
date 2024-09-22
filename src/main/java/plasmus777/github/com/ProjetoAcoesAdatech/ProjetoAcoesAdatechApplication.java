bc055627ebc055b3e4b1638be9e48f8ee482c549
plasmus777 <118869201+plasmus777@users.noreply.github.com>
2024-09-22 17:51:11 -0300

    Upload Inicial do Projeto
    Envia o projeto gerado com o Spring Initializr, configurado com as dependências Maven necessárias

---
+++ src/main/java/plasmus777/github/com/ProjetoAcoesAdatech/ProjetoAcoesAdatechApplication.java
@@ -0,0 +1,13 @@
+package plasmus777.github.com.ProjetoAcoesAdatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcoesAdatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcoesAdatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plasmus777/github/com/ProjetoAcoesAdatech/ProjetoAcoesAdatechApplicationTests.java
@@ -0,0 +1,13 @@
+package plasmus777.github.com.ProjetoAcoesAdatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcoesAdatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
