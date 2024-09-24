c4a1f4afc636289f113cd332e9428349ada8f2ae
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-24 00:15:54 -0300

    Inicializa a aplicação Spring Boot

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/main/ProjetoAcoesAdatechApplication.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/main/ProjetoAcoesAdatechApplication.java
@@ -2,12 +2,13 @@ package plasmus777.github.com.projetoAcoesAdatech.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProjetoAcoesAdatechApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoAcoesAdatechApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoAcoesAdatechApplication.class, args);
+    }
 }
