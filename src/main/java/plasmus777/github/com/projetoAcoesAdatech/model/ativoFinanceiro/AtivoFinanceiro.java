2900d5654766de518dd069adff75d69caf9af950
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-24 00:39:20 -0300

    Define entidade base AtivoFinanceiro

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiro/AtivoFinanceiro.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiro/AtivoFinanceiro.java
@@ -1,4 +1,34 @@
 package plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiro;
 
-public class AtivoFinanceiro {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@MappedSuperclass
+public abstract class AtivoFinanceiro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column(nullable = false)
+    private BigDecimal valorAtual;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate dataCadastro;
+
+    @Column
+    private Boolean favorito = false;
+
+    @Column
+    private BigDecimal precoCompra;
+
+    @Column
+    private BigDecimal precoVenda;
 }
