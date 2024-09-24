006dbb117b83a737f27fb178e97de21f77322ad1
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-24 00:35:06 -0300

    Define entidade FundoImobiliario para ativos financeiros

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiro/FundoImobiliario.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiro/FundoImobiliario.java
@@ -1,4 +1,11 @@
 package plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiro;
 
-public class FundoImobiliario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "fundo_imobiliario")
+public class FundoImobiliario extends AtivoFinanceiro {
 }
