ed9337b115f9f3c5628ef6b2413e64d73c2af57b
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-25 13:40:05 -0300

    Criado o serviço de envio de emails em EmailService

---
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.plasmus777.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void enviarEmail(String para, String assunto, String corpo) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(para);
+        message.setSubject(assunto);
+        message.setText(corpo);
+        message.setFrom("compraevendas.acoes@gmail.com");
+
+        mailSender.send(message);
+    }
+}
