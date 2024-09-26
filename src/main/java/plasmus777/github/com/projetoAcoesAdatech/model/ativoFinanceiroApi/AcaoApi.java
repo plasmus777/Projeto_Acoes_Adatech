b20bc0fec821568cc7fe24cc09e9c24200467f8f
plasmus777 <118869201+plasmus777@users.noreply.github.com>
2024-09-25 22:36:45 -0300

    Mudança de Variáveis em AcaoApi
    As alterações nos nomes das variáveis utilizando @JsonProperty torna mais fácil identificar os valores para uso.

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiroApi/AcaoApi.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/ativoFinanceiroApi/AcaoApi.java
@@ -1,17 +1,26 @@
 package plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiroApi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class AcaoApi {
-    private BigDecimal c; //Current price
-    private BigDecimal d; //Change
-    private BigDecimal dp; //Percent change
-    private BigDecimal h; //High price of the day
-    private BigDecimal l; //Low price of the day
-    private BigDecimal o; //Open price of the day
-    private BigDecimal pc; //Previous close price
-    private Long t; //Timestamp
+    @JsonProperty("c")
+    private BigDecimal precoAtual; //Current price
+    @JsonProperty("d")
+    private BigDecimal alteracao; //Change
+    @JsonProperty("dp")
+    private BigDecimal porcentagemAlteracao; //Percent change
+    @JsonProperty("h")
+    private BigDecimal maiorPrecoDiario; //High price of the day
+    @JsonProperty("l")
+    private BigDecimal menorPrecoDiario; //Low price of the day
+    @JsonProperty("o")
+    private BigDecimal precoAbertura; //Open price of the day
+    @JsonProperty("pc")
+    private BigDecimal precoFechamentoAnterior; //Previous close price
+    @JsonProperty("t")
+    private Long timestamp; //Timestamp
 }
--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/service/EmailService.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/service/EmailService.java
@@ -1,6 +1,5 @@
 package plasmus777.github.com.projetoAcoesAdatech.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,8 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
 
     public void enviarEmail(String para, String assunto, String corpo) {
         SimpleMailMessage message = new SimpleMailMessage();
