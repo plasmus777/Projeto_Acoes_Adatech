4385e978818357095574d8ab7929c8f79746ac45
Luuqee <www.luucas@gmail.com>
2024-09-26 16:02:45 -0300

    Aprimoramento das Validações FundoImobiliarioDTO e AcaoDTO

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/AcaoDTO.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/AcaoDTO.java
@@ -1,16 +1,20 @@
 package plasmus777.github.com.projetoAcoesAdatech.model.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiro.Acao;
 
 public class AcaoDTO implements DTO<AcaoDTO, Acao>{
 
 @NotNull(message = "O código de negociação não pode ser nulo.")
+@Pattern(regexp = "^[A-Z 0-9]{1,5}$", message = "O código de negociação deve conter apenas letras e números, com até 5 caracteres.")
 @Size(min=1, max=5, message = "O código de negociação deve ter entre 1 a 5 caracteres.")
 private String codigoNegociacao;
 
 @NotNull(message = "A quantidade de ações não pode ser nula.")
+@Min(value = 1, message = "A quantidade deve ser pelo menos 1.")
 private Integer quantidade;
 
 
--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/FundoImobiliarioDTO.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/FundoImobiliarioDTO.java
@@ -1,17 +1,22 @@
 package plasmus777.github.com.projetoAcoesAdatech.model.dto;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiro.FundoImobiliario;
 
 public class FundoImobiliarioDTO {
 
+    //perguntar professor
     @NotNull(message = "O código do FII não pode ser nulo.")
+    @Pattern(regexp = "^[A-Z0-9]{1,10}$", message = "O código do FII deve conter apenas letras e números, com até 10 caracteres.")
     @Size(min = 1, max = 10, message = "O código do FII deve ter entre 1 e 10 caracteres.")
     private String codigoFii;
 
     @NotNull(message = "O rendimento mensal não pode ser nulo.")
+    @DecimalMin(value = "0.00", inclusive = false, message = "O rendimento mensal deve ser um valor positivo.")
     private BigDecimal rendimentoMensal;
 
 
