2d8ae5914392947f1590b36d29b179cae8d4beff
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-09-26 22:43:36 -0300

    Atualizar restrições de validação e corrigir nomes de métodos

--- src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/FundoImobiliarioDTO.java
+++ src/main/java/plasmus777/github/com/projetoAcoesAdatech/model/dto/FundoImobiliarioDTO.java
@@ -9,7 +9,6 @@ import plasmus777.github.com.projetoAcoesAdatech.model.ativoFinanceiro.FundoImob
 
 public class FundoImobiliarioDTO {
 
-    //perguntar professor
     @NotNull(message = "O código do FII não pode ser nulo.")
     @Pattern(regexp = "^[A-Z0-9]{1,10}$", message = "O código do FII deve conter apenas letras e números, com até 10 caracteres.")
     @Size(min = 1, max = 10, message = "O código do FII deve ter entre 1 e 10 caracteres.")
@@ -19,35 +18,29 @@ public class FundoImobiliarioDTO {
     @DecimalMin(value = "0.00", inclusive = false, message = "O rendimento mensal deve ser um valor positivo.")
     private BigDecimal rendimentoMensal;
 
-
     public FundoImobiliarioDTO() {
     }
-
 
     public FundoImobiliarioDTO(String codigoFii, BigDecimal rendimentoMensal) {
         this.codigoFii = codigoFii;
         this.rendimentoMensal = rendimentoMensal;
     }
 
-   //gs
-
     public String getCodigoFii() {
         return codigoFii;
-    }
-
-    public BigDecimal getRendimentoMensal() {
-        return rendimentoMensal;
     }
 
     public void setCodigoFii(String codigoFii) {
         this.codigoFii = codigoFii;
     }
 
+    public BigDecimal getRendimentoMensal() {
+        return rendimentoMensal;
+    }
+
     public void setRendimentoMensal(BigDecimal rendimentoMensal) {
         this.rendimentoMensal = rendimentoMensal;
     }
-
-    //
 
     public FundoImobiliario toEntity() {
         FundoImobiliario fundoImobiliario = new FundoImobiliario();
